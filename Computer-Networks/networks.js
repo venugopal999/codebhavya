1e6b0fa7c837fcf842d5a5a669da699d80200ef1
venugopal999 <venugopal.gora@gmail.com>
2026-09-26 23:34:01 +0530

    Add files via upload

--- Computer-Networks/networks.js
+++ Computer-Networks/networks.js
@@ -59,10 +59,3 @@
   }
 })();
 
-/* Load the shared mobile course drawer. */
-if (!document.querySelector('script[data-cb-course-drawer]')) {
-  const courseDrawer = document.createElement('script');
-  courseDrawer.src = '../course-mobile-sidebar.js?v=3';
-  courseDrawer.dataset.cbCourseDrawer = '';
-  document.head.append(courseDrawer);
-}
